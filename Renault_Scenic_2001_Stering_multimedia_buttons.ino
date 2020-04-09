d525e10ec94c234117da93ad8158d7b943735a73
daskalovst <daskalov_st@abv.bg>
2020-04-09 11:31:30 +0300

    Update and rename reno-v1.ino to Renault_Scenic_2001_Stering_multimedia_buttons.ino

--- Renault_Scenic_2001_Stering_multimedia_buttons.ino
+++ Renault_Scenic_2001_Stering_multimedia_buttons.ino
@@ -1,3 +1,5 @@
+#define DEBUG
+
 const int r6 = 13;
 const int r5 = 12;
 const int r4 = 14;
@@ -17,9 +19,11 @@ int first = 1;
 void setup() {
   int one = 1;
 
- delay(50);
+ delay(10);
  Serial.begin(115200);
+ #ifdef DEBUG 
  Serial.println("Port test");
+ #endif
  
 }
 
@@ -33,7 +37,7 @@ void loop() {
 
 digitalWrite(r1, LOW);
 
-delay(50);
+//delay(10); //to check without
 if(digitalRead(r1) == 0) {
 if((digitalRead(r4)) == 0) {
   Serial.println("Mute");
@@ -43,7 +47,7 @@ if((digitalRead(r6)) == 0) {
 }
 digitalWrite(r1, HIGH);
 }
-delay(50);
+//delay(10); //to check without
 
  digitalWrite(r3, LOW);
 
@@ -57,24 +61,22 @@ if((digitalRead(r6)) == 0) {
 }
 }
 digitalWrite(r3, HIGH);
-delay(50);
+//delay(10); //to check without
 
  digitalWrite(r5, LOW);
 
-delay(50);
+delay(10); //to check without
 if(digitalRead(r5) ==0) {
 if((digitalRead(r4)) == 0) {
   Serial.println("SR");
 }
-//if((digitalRead(r6)) == 0) {
-//  Serial.println("Shift");
-//}
+
 digitalWrite(r5, HIGH);
 
 }
 pinMode(r5, INPUT_PULLUP);
 pinMode(r6, OUTPUT);
-delay(50);
+delay(10);
 digitalWrite(r6, LOW);
 
 if((digitalRead(r5)) == 0) {
@@ -87,7 +89,7 @@ if((digitalRead(r2)) == 0) {
 digitalWrite(r6, HIGH);
 
 
-delay(50);
+//delay(10); //to check without
 
  
  //Encoder
@@ -95,70 +97,80 @@ delay(50);
  pinMode(r3, INPUT_PULLUP);
  pinMode(r5, INPUT_PULLUP);
  pinMode(r2, OUTPUT);
- pinMode(r4, OUTPUT);
- pinMode(r6, OUTPUT);
- delay(50);
+ delay(10);  
  digitalWrite(r2, LOW);
- delay(10);
+ //delay(10); //to check without
 
  c1 = digitalRead(r1);
  c3 = digitalRead(r3);
  c5 = digitalRead(r5);
- 
+
+ #ifdef DEBUG
  Serial.print("RAW_R1=");
- Serial.println((r1));
+ Serial.println((c1));
  Serial.print("RAW_R3=");
- Serial.println((r3));
+ Serial.println((c3));
  Serial.print("RAW_R5=");
- Serial.println((r5));
+ Serial.println((c5));
+ #endif
  
 if(c1 == 0) { 
   if(c1 != c1old) {
-    Serial.println("if c1");
-    if (c3old = 0) {
+    #ifdef DEBUG
+  Serial.println("if c1");
+  #endif
+    if (c3old == 0) {
        Serial.println("UP");
        }
-    if (c5old = 0) {
+    if (c5old == 0) {
        Serial.println("Down");
        }
    }
+c1old = c1;
+c3old = c3;
+c5old = c5;
 }
 
 if(c3 == 0) {
   if(c3 != c3old) {
-    Serial.println("if c3");
-    if (c1old = 0) {
+    #ifdef DEBUG
+     Serial.println("if c3");
+     #endif
+    if (c1old == 0) {
        Serial.println("Down");
        }
-    if (c3old = 0) {
+    if (c5old == 0) {
        Serial.println("UP");
        }
     }
+c1old = c1;
+c3old = c3;
+c5old = c5;
  }
 
  if(c5 == 0) {
    if(c5 != c5old) {
-     Serial.println("if c5");
-     if (c3old = 0) {
-        Serial.println("UP");
-     }
-     if (c5old = 0) {
+    #ifdef DEBUG
+      Serial.println("if c5");
+      #endif
+     if (c3old == 0) {
         Serial.println("Down");
      }
+     if (c1old == 0) {
+        Serial.println("UP");
+     }
    }
- }
- 
 c1old = c1;
 c3old = c3;
 c5old = c5;
- 
+ }
+
+#ifdef DEBUG
 Serial.print("OLD_R1=");
 Serial.println((c1old));
 Serial.print("OLD_R3=");
 Serial.println((c3old));
 Serial.print("OLD_R5=");
 Serial.println((c5old));
- 
- delay(500);
-
+#endif
 }
